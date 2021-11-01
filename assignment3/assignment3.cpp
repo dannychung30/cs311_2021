4d64b69a696a8fd59a777b0fd96d60588e83a6bc
dannychung30 <90223301+dannychung30@users.noreply.github.com>
2021-10-31 17:54:10 -0700

    Update assignment3.cpp

--- assignment3/assignment3.cpp
+++ assignment3/assignment3.cpp
@@ -18,36 +18,39 @@ int main(int argc, char* last_opponent_move[])
     // first iteration of game will have "zero" passed through as opponent's move
     //
     // tit for tat strategy
-    if (argc >= 2)
+    if (argc >= 3)
     {
-        if (string(last_opponent_move[1]) == "confess")
+        if(string(last_opponent_move[1] == "--last_opponent_move")
         {
-            // prints out "confess" if last opponent's move was "confess"
-            last_opponent_moves.push(last_opponent_move[1]);
-            my_moves.push(string(last_opponent_move[1]));
-            cout << my_moves.top() << endl;
-        }
-        else if (string(last_opponent_move[1]) == "silent")
-        {
-            // prints out "silent" if last opponent's move was "silent"
-            last_opponent_moves.push(last_opponent_move[1]);
-            my_moves.push(string(last_opponent_move[1]));
-            cout << my_moves.top() << endl;
-            
-        }
-        else if (string(last_opponent_move[1]) == "zero")
-        {
-            // output "silent" if opponent's move was "zero"
-            // should only run on first iteration of the game
-            choice.push_back("silent");
-            for (int i = 0; i < choice.size(); i++)
+            if (string(last_opponent_move[2]) == "confess")
             {
-                cout << choice[i] << endl;
+                // prints out "confess" if last opponent's move was "confess"
+                last_opponent_moves.push(last_opponent_move[2]);
+                my_moves.push(string(last_opponent_move[2]));
+                cout << my_moves.top() << endl;
+            }
+            else if (string(last_opponent_move[2]) == "silent")
+            {
+                // prints out "silent" if last opponent's move was "silent"
+                last_opponent_moves.push(last_opponent_move[2]);
+                my_moves.push(string(last_opponent_move[2]));
+                cout << my_moves.top() << endl;
+                
+            }
+            else if (string(last_opponent_move[2]) == "zero")
+            {
+                // output "silent" if opponent's move was "zero"
+                // should only run on first iteration of the game
+                choice.push_back("silent");
+                for (int i = 0; i < choice.size(); i++)
+                {
+                    cout << choice[i] << endl;
+                }
             }
         }
 
     }
-    if (argc < 2)
+    if (argc < 3)
     {
         // outputs this message if neither "confess", "silent", or "zero" are not inputed
         // into the command line prompt
